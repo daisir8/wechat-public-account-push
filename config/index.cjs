87157983b2952a579e97411fdf62aec12efd39df
daisir8 <67043197+daisir8@users.noreply.github.com>
2024-11-16 16:48:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,7 +30,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'odVvL53bgw0J-TKb8-pyTzY3miK4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vYHqepoMFxd6c8P8CA65X_cuOpzmJrxZdESUZZPVwps',
+      useTemplateId: 'uZ6RayyR2sIFZWOLYJrNAVGVU3X576pxKTGqEAiT98E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '6-26',
       festivals: [
@@ -64,7 +64,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'x',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'vYHqepoMFxd6c8P8CA65X_cuOpzmJrxZdESUZZPVwps',
+      useTemplateId: 'uZ6RayyR2sIFZWOLYJrNAVGVU3X576pxKTGqEAiT98E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-25',
       festivals: [
