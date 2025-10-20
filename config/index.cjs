a9fff7e60862af86f52616ca7f9a02933b3e2b63
daisir8 <67043197+daisir8@users.noreply.github.com>
2025-10-20 15:39:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,17 +6,32 @@
  * 自行添加属性，以支持更多个性化功能
  */
 
- TIAN_API: {
+TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: '6164d804c076e60c3f2b6b4094acaa5c'
+    key: '6164d804c076e60c3f2b6b4094acaa5c',
     
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
   }
 const USER_CONFIG = 
  {
 	"APP_ID": "wxc50473b3d6111ffd",
 	"APP_SECRET": "dc4a3c9bf39d2fa2521328d5c37c1952",
 	"IS_SHOW_COLOR": true,
-	"CALLBACK_TEMPLATE_ID": "w63EcQatMRpy2hRJLkNJgqjkVp6IbC64iYkmTVvO2ek",
+	"CALLBACK_TEMPLATE_ID": "eL96rPEh4VzBnoWV2MppshQaFLr_aoV3UfRA2-tA4TI",
 	"CALLBACK_USERS": [{
 		"name": "自己",
 		"id": "oUcwA2GItAzBzrGQ_L124NXV3-YU"
@@ -24,7 +39,7 @@ const USER_CONFIG =
 	"USERS": [{
 		"name": "小竹",
 		"id": "oUcwA2GItAzBzrGQ_L124NXV3-YU",
-		"useTemplateId": "jXcvDcCZa7rUkwePV3Xxd-SVdS0oqMt5Wp4gvW4aAR8",
+		"useTemplateId": "eL96rPEh4VzBnoWV2MppshQaFLr_aoV3UfRA2-tA4TI",
 		"province": "福建",
 		"city": "晋江",
 		"horoscopeDate": "02-14",
