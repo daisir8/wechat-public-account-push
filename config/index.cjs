9e758b7970ba4dff77a46a857826dddecbc1d037
daisir8 <67043197+daisir8@users.noreply.github.com>
2025-10-20 14:23:05 +0800

    2

--- config/index.cjs
+++ config/index.cjs
@@ -11,5 +11,49 @@
     key: '6164d804c076e60c3f2b6b4094acaa5c'
     
   }
-const USER_CONFIG = {"APP_ID":"wxc50473b3d6111ffd","APP_SECRET":"dc4a3c9bf39d2fa2521328d5c37c1952","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"eL96rPEh4VzBnoWV2MppshQaFLr_aoV3UfRA2-tA4TI","CALLBACK_USERS":[{"name":"自己","id":"oUcwA2GItAzBzrGQ_L124NXV3-YU"}],"USERS":[{"name":"小竹","id":"oUcwA2GItAzBzrGQ_L124NXV3-YU","useTemplateId":"eL96rPEh4VzBnoWV2MppshQaFLr_aoV3UfRA2-tA4TI","province":"福建","city":"晋江","horoscopeDate":"02-14","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"生日快乐~","date":"02-14","year":"2004"}],"customizedDateList":[{"keyword":"认识纪念日","date":"2025-10-03"}]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
-module.exports = USER_CONFIG
+const USER_CONFIG = 
+ {
+	"APP_ID": "wxc50473b3d6111ffd",
+	"APP_SECRET": "dc4a3c9bf39d2fa2521328d5c37c1952",
+	"IS_SHOW_COLOR": true,
+	"CALLBACK_TEMPLATE_ID": "w63EcQatMRpy2hRJLkNJgqjkVp6IbC64iYkmTVvO2ek",
+	"CALLBACK_USERS": [{
+		"name": "自己",
+		"id": "oUcwA2GItAzBzrGQ_L124NXV3-YU"
+	}],
+	"USERS": [{
+		"name": "小竹",
+		"id": "oUcwA2GItAzBzrGQ_L124NXV3-YU",
+		"useTemplateId": "w63EcQatMRpy2hRJLkNJgqjkVp6IbC64iYkmTVvO2ek",
+		"province": "福建",
+		"city": "晋江",
+		"horoscopeDate": "02-14",
+		"horoscopeDateType": "今日",
+		"openUrl": "https://shuangxunian.github.io/",
+		"festivals": [{
+			"type": "生日",
+			"name": "生日快乐~",
+			"date": "02-14",
+			"year": "2004"
+		}],
+		"customizedDateList": [{
+			"keyword": "认识纪念日",
+			"date": "2025-10-03"
+		}]
+	}],
+	"SWITCH": {
+		"weather": true,
+		"holidaytts": true,
+		"CIBA": true,
+		"oneTalk": false,
+		"earthyLoveWords": false,
+		"momentCopyrighting": false,
+		"poisonChickenSoup": false,
+		"poetry": false,
+		"horoscope": false,
+		"birthdayMessage": true
+	}
+}
+ 
+ 
+ module.exports = USER_CONFIG
